55c1f3ac8b9bf2316c1c528ddc95e6eeba8b12b9
Kasia Izak <kontakt@kasiaizak.pl>
2024-05-07 12:52:13 +0200

    Add userscript for LastPass

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-### Install _script manager_ in your browser
+# How to use?
+
+## 1. Install _script manager_ in your browser
 
 For example:
 
@@ -6,12 +8,18 @@ For example:
 * [Violentmonkey](https://violentmonkey.github.io/) for Chrome, Firefox, Edge, Opera, Vivaldi and more Chromium based browsers
 * [Greasemonkey](https://www.greasespot.net/) for Firefox
 
-### What next?
+## 2. What next?
 
 * Open a script you are interested in and install it in your _script manager_.
 
-## WordPress Customize Panel Resizer
+### WordPress Customize Panel Resizer
 
 Userscript to change the minimal width of the customization panel in WordPress.
 
 [Click here to install: WP Customize Panel Resizer](https://raw.githubusercontent.com/kasiaizak/userscripts/master/wp-customize-panel-resizer.user.js)
+
+### Resizer for LastPass
+
+Changes the maximum width of the Edit Identity Popup
+
+[Click here to install: Resizer for LastPass](https://raw.githubusercontent.com/kasiaizak/userscripts/master/lastpass-resizer.user.js)
---
+++ lastpass-resizer.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Resizer for LastPass
+// @namespace    https://kasiaizak.github.io/userscripts/
+// @version      1.0
+// @description  Changes the maximum width of the Edit Identity Popup
+// @author       Kasia Izak
+// @match        https://lastpass.com/vault/
+// @updateURL    https://raw.githubusercontent.com/kasiaizak/userscripts/master/lastpass-resizer.user.js
+// @downloadURL  https://raw.githubusercontent.com/kasiaizak/userscripts/master/lastpass-resizer.user.js
+// @homepageURL  https://kasiaizak.github.io/userscripts/#resizer-for-lastpass
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function panelResizer(css) {
+        const head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+    panelResizer('#identityDialog.dialog.responsive {max-width: 90vw;}');
+})();
