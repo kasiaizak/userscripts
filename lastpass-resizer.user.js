ed61fdd29fc72c93304d6a0920366daaf7463b2c
Kasia Izak <kontakt@kasiaizak.pl>
2024-10-28 13:20:02 +0100

    fix: change 'match' for lastpass-resizer

--- lastpass-resizer.user.js
+++ lastpass-resizer.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Resizer for LastPass
 // @namespace    https://kasiaizak.github.io/userscripts/
-// @version      1.0
+// @version      1.1
 // @description  Changes the maximum width of the Edit Identity Popup
 // @author       Kasia Izak
-// @match        https://lastpass.com/vault/
+// @match        https://*.lastpass.com/*
 // @updateURL    https://raw.githubusercontent.com/kasiaizak/userscripts/master/lastpass-resizer.user.js
 // @downloadURL  https://raw.githubusercontent.com/kasiaizak/userscripts/master/lastpass-resizer.user.js
 // @homepageURL  https://kasiaizak.github.io/userscripts/#resizer-for-lastpass
